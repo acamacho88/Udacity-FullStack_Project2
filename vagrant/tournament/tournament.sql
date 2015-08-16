6f54c3e4427b4d108d8811c59c2829e057ac878d
acamacho <billyshears06@gmail.com>
2015-08-15 22:44:25 -0700

    Created views for winners, losers, and total matches

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -9,4 +9,17 @@
 
 CREATE TABLE players (id serial primary key, name text);
 
-CREATE TABLE matches (match_id serial, winner integer references players (id), loser integer references players (id));
+CREATE TABLE matches (match_id serial, winner integer references players (id),
+                      loser integer references players (id));
+
+CREATE VIEW wins as SELECT id, COUNT(winner) wins FROM matches
+RIGHT JOIN players ON winner = id
+GROUP BY id;
+
+CREATE VIEW losses as SELECT id, COUNT(loser) losses FROM matches
+RIGHT JOIN players ON loser = id
+GROUP BY id;
+
+CREATE VIEW tot_matches as SELECT id, COUNT(winner) total FROM matches
+RIGHT JOIN players ON id = winner OR id = loser
+GROUP BY id;
